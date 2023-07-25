dae4a05b760aa7e17259a4a76618ef75668c8ad6
a.tafreshi <alitafreshi123@yahoo.com>
2023-07-25 08:54:32 +0330

    done

--- home/presentation/src/main/java/com/tafreshiali/presentation/components/HomeTrendingImageSliderComponent.kt
+++ home/presentation/src/main/java/com/tafreshiali/presentation/components/HomeTrendingImageSliderComponent.kt
@@ -1,6 +1,7 @@
 package com.tafreshiali.presentation.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
@@ -21,9 +22,7 @@ import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import com.tafreshiali.domain.model.Result
 import com.tafreshiali.presentation.HomeConstance
-import kotlin.math.PI
 import kotlin.math.absoluteValue
-import kotlin.math.sin
 
 @ExperimentalFoundationApi
 @Composable
@@ -47,27 +46,9 @@ fun HomeImageSliderComponent(modifier: Modifier = Modifier, imageUrls: List<Resu
                 (pagerState.currentPage - index) + pagerState
                     .currentPageOffsetFraction
                 ).absoluteValue
-        val curvedAngle = calculateCurvedAngle(scrollOffset = pageOffset)
-        val angle = if (index < pagerState.currentPage) {
-            curvedAngle + 2 * PI.toFloat() / imageUrls.size * index // Previous items in the first quadrant
-        } else {
-            curvedAngle + 2 * PI.toFloat() / imageUrls.size * (index - imageUrls.size) // Next items in the second quadrant
-        }
-
-        val rotationZ = if (index < pagerState.currentPage) {
-            (-angle).toDegrees().coerceIn(-10f, 0f) // Previous items rotate in the first quadrant
-        } else {
-            (-angle).toDegrees().coerceIn(0f, 10f) // Next items rotate in the second quadrant
-        }
-
-
-        //val angle = curvedAngle + 2 * PI.toFloat() / imageUrls.size * index
-
         ImageSlideComponent(
             imageUrl = imageUrls[index].posterPath,
-            pageOffset = pageOffset,
-            angle = rotationZ,
-            isCurrentItem = !(index < pagerState.currentPage || index > pagerState.currentPage)
+            pageOffset = pageOffset
         )
     }
 }
@@ -77,8 +58,6 @@ private fun ImageSlideComponent(
     modifier: Modifier = Modifier,
     imageUrl: String,
     pageOffset: Float,
-    angle: Float,
-    isCurrentItem: Boolean
 ) {
     SubcomposeAsyncImage(
         model = "${HomeConstance.IMAGE_BASE_URL}/w780$imageUrl",
@@ -94,15 +73,12 @@ private fun ImageSlideComponent(
                     scaleX = scale
                     scaleY = scale
                 }
-                /* alpha = lerp(
-                     start = 0.5f,
-                     stop = 1f,
-                     fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                 )*/
-                rotationZ =
-                    lerp(start = angle, stop = 0f, fraction = 1f - pageOffset.coerceIn(0f, 1f))
+                alpha = lerp(
+                    start = 0.5f,
+                    stop = 1f,
+                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                )
             }
-            .zIndex(if (isCurrentItem) 2f else 0f)
             .clip(shape = MaterialTheme.shapes.large)
     ) {
         when (painter.state) {
@@ -116,14 +92,6 @@ private fun ImageSlideComponent(
         }
     }
 }
-
-fun calculateCurvedAngle(scrollOffset: Float): Float {
-    val curveFactor = 0.2f // Adjust this value to control the curvature
-    val angle = scrollOffset * curveFactor
-    return sin(angle)
-}
-
-fun Float.toDegrees(): Float = (this * 180f / PI).toFloat()
 
 @ExperimentalFoundationApi
 @Preview
