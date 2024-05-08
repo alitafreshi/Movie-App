e69ee9e5229eb05c1b6b8cbef7a16d2640e4009e
alitafreshi <alitafreshi123@yahoo.com>
2024-05-08 20:19:32 +0330

    send requests in parallel

--- home/data/src/main/java/com/tafreshiali/data/remote/HomeMoviesRepositoryImpl.kt
+++ home/data/src/main/java/com/tafreshiali/data/remote/HomeMoviesRepositoryImpl.kt
@@ -10,18 +10,26 @@ import com.tafreshiali.domain.repository.HomeMoviesRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.request.prepareGet
 import io.ktor.http.appendEncodedPathSegments
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class HomeMoviesRepositoryImpl @Inject constructor(private val httpClient: HttpClient) :
     HomeMoviesRepository {
     override suspend fun getTrendingMovies(type: String, time: String): BaseMovieIntro {
-        val trendingMovies: BaseMovieIntroResponse = httpClient.prepareGet {
-            url {
-                appendEncodedPathSegments("trending", type, time)
+        return coroutineScope {
+            val trendingMovies: Deferred<BaseMovieIntroResponse> = async {
+                httpClient.prepareGet {
+                    url {
+                        appendEncodedPathSegments("trending", type, time)
+                    }
+                }.body()
             }
-        }.body()
-        val movieGenres: List<MovieGenresItem> = getGenres()
-        return trendingMovies.toBaseMovieIntro(genreList = movieGenres)
+            val movieGenres: Deferred<List<MovieGenresItem>> = async { getGenres() }
+            trendingMovies.await().toBaseMovieIntro(movieGenres.await())
+        }
     }
 
     private suspend fun getGenres(): List<MovieGenresItem> {
