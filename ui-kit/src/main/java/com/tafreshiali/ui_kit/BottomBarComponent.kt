54b18e2dc1d2bf8c7fd1289c7423443e7d5a6242
alitafreshi <alitafreshi123@yahoo.com>
2024-05-10 23:40:56 +0330

    WIP Improve Bottom Bar

--- ui-kit/src/main/java/com/tafreshiali/ui_kit/BottomBarComponent.kt
+++ ui-kit/src/main/java/com/tafreshiali/ui_kit/BottomBarComponent.kt
@@ -7,15 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,20 +27,16 @@ import androidx.compose.ui.unit.dp
 import com.tafreshiali.ui_kit.design_system.ui.theme.AppTheme
 import com.tafreshiali.ui_kit.design_system.ui.theme.primaryLight
 
+data class BottomNavItem(
+    val icon: VectorPainter,
+    val isSelected: Boolean = false,
+    val selectedColor: Color,
+    val unSelectedColor: Color
+)
+
 @Composable
-fun BottomBarComponent(modifier: Modifier = Modifier) {
-
-    val homeIcon =
-        rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_home_navigation))
-
-    val searchIcon =
-        rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_search_navigation))
-
-    val downloadIcon =
-        rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_download_navigation))
-
-    val profileIcon =
-        rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_profile_navigation))
+fun BottomBarComponent(modifier: Modifier = Modifier, items: List<BottomNavItem>) {
+    val navItems = rememberSaveable { mutableStateOf(items) }
 
     Canvas(
         modifier = modifier
@@ -44,123 +44,37 @@ fun BottomBarComponent(modifier: Modifier = Modifier) {
             .height(74.dp)
             .shadow(1.dp)
             .drawWithCache {
-                val sectionWidth = (size.width / 4)
+                val sectionWidth = (size.width / navItems.value.size)
                 val sectionHeight = size.height / 2
                 val centerOfEachSection = sectionWidth / 2
-
                 onDrawBehind {
-
-                   /* drawRect(
-                        color = Color.Black,
-                        topLeft = Offset(
-                            x = 0f,
-                            y = sectionHeight - homeIcon.intrinsicSize.height / 2
-                        ),
-                        size = Size(sectionWidth, sectionHeight - homeIcon.intrinsicSize.height / 2)
-                    )
-
-                    drawRect(
-                        color = Color.Blue,
-                        topLeft = Offset(
-                            x = sectionWidth,
-                            y = sectionHeight - searchIcon.intrinsicSize.height / 2
-                        ),
-                        size = Size(
-                            sectionWidth,
-                            sectionHeight - searchIcon.intrinsicSize.height / 2
-                        )
-                    )
-
-                    drawRect(
-                        color = Color.Red,
-                        topLeft = Offset(
-                            x = sectionWidth * 2,
-                            y = sectionHeight - searchIcon.intrinsicSize.height / 2
-                        ),
-                        size = Size(
-                            sectionWidth,
-                            sectionHeight - searchIcon.intrinsicSize.height / 2
-                        )
-                    )
-
-                    drawRect(
-                        color = Color.Green,
-                        topLeft = Offset(
-                            x = sectionWidth * 3,
-                            y = sectionHeight - searchIcon.intrinsicSize.height / 2
-                        ),
-                        size = Size(
-                            sectionWidth,
-                            sectionHeight - searchIcon.intrinsicSize.height / 2
-                        )
-                    )*/
-
-                    //HOME ICON
-                    translate(
-                        centerOfEachSection - (homeIcon.intrinsicSize.width / 2),
-                        sectionHeight - homeIcon.intrinsicSize.height / 2
-                    ) {
-                        with(homeIcon) {
-                            draw(homeIcon.intrinsicSize)
+                    navItems.value.forEachIndexed { index, bottomNavItem ->
+                        translate(
+                            left = ((index * sectionWidth) + centerOfEachSection) - (bottomNavItem.icon.intrinsicSize.width / 2),
+                            top = sectionHeight - bottomNavItem.icon.intrinsicSize.height / 2
+                        ) {
+                            with(bottomNavItem.icon) {
+                                draw(
+                                    size = bottomNavItem.icon.intrinsicSize,
+                                    colorFilter = ColorFilter.tint(if (bottomNavItem.isSelected) bottomNavItem.selectedColor else bottomNavItem.unSelectedColor)
+                                )
+                            }
                         }
-                    }
 
-                    drawLine(
-                        color = primaryLight,
-                        start = Offset(
-                            centerOfEachSection - (homeIcon.intrinsicSize.width / 2),
-                            (homeIcon.intrinsicSize.width * 2) + 6.dp.toPx()
-                        ),
-                        end = Offset(
-                            centerOfEachSection - (homeIcon.intrinsicSize.width / 2) + homeIcon.intrinsicSize.width,
-                            (homeIcon.intrinsicSize.width * 2) + 6.dp.toPx()
-                        ),
-                        strokeWidth = 2.dp.toPx(),
-                        cap = StrokeCap.Round
-                    )
-
-
-                    //SEARCH ICON
-                    translate(
-                        (sectionWidth + centerOfEachSection) - (searchIcon.intrinsicSize.width / 2),
-                        sectionHeight - searchIcon.intrinsicSize.height / 2
-                    ) {
-                        with(searchIcon) {
-                            draw(searchIcon.intrinsicSize)
-                        }
-                    }
-
-                    drawLine(
-                        color = primaryLight,
-                        start = Offset(
-                            (sectionWidth + centerOfEachSection) - (searchIcon.intrinsicSize.width / 2),
-                            (homeIcon.intrinsicSize.width * 2) + 6.dp.toPx()
-                        ),
-                        end = Offset(
-                            (sectionWidth + centerOfEachSection) - (searchIcon.intrinsicSize.width / 2) +searchIcon.intrinsicSize.width ,
-                            (homeIcon.intrinsicSize.width * 2) + 6.dp.toPx()
-                        ),
-                        strokeWidth = 2.dp.toPx(),
-                        cap = StrokeCap.Round
-                    )
-
-                    //DOWNLOAD ICON
-                    translate(
-                        (sectionWidth * 2 + centerOfEachSection) - (downloadIcon.intrinsicSize.width / 2),
-                        sectionHeight - downloadIcon.intrinsicSize.height / 2
-                    ) {
-                        with(downloadIcon) {
-                            draw(downloadIcon.intrinsicSize)
-                        }
-                    }
-
-                    //PROFILE ICON
-                    translate(
-                        (sectionWidth * 3 + centerOfEachSection) - (profileIcon.intrinsicSize.width / 2),
-                        sectionHeight - profileIcon.intrinsicSize.height / 2
-                    ) {
-                        with(profileIcon) {
-                            draw(profileIcon.intrinsicSize)
+                        if (bottomNavItem.isSelected) {
+                            drawLine(
+                                color = bottomNavItem.selectedColor,
+                                start = Offset(
+                                    ((index * sectionWidth) + centerOfEachSection) - (bottomNavItem.icon.intrinsicSize.width / 2),
+                                    (bottomNavItem.icon.intrinsicSize.height * 2) + 6.dp.toPx()
+                                ),
+                                end = Offset(
+                                    ((index * sectionWidth) + centerOfEachSection) - (bottomNavItem.icon.intrinsicSize.width / 2) + bottomNavItem.icon.intrinsicSize.width,
+                                    (bottomNavItem.icon.intrinsicSize.height * 2) + 6.dp.toPx()
+                                ),
+                                strokeWidth = 2.dp.toPx(),
+                                cap = StrokeCap.Round
+                            )
                         }
                     }
                 }
@@ -174,6 +88,32 @@ fun BottomBarComponent(modifier: Modifier = Modifier) {
 @Composable
 private fun BottomBarComponentPreview() {
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
-        BottomBarComponent()
+        BottomBarComponent(
+            items = listOf(
+                BottomNavItem(
+                    icon = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_home_navigation)),
+                    isSelected = false,
+                    selectedColor = primaryLight,
+                    unSelectedColor = Color.Gray
+                ),
+                BottomNavItem(
+                    icon = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_search_navigation)),
+                    isSelected = false,
+                    selectedColor = primaryLight,
+                    unSelectedColor = Color.Gray
+                ), BottomNavItem(
+                    icon = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_download_navigation)),
+                    isSelected = false,
+                    selectedColor = primaryLight,
+                    unSelectedColor = Color.Gray
+                ),
+                BottomNavItem(
+                    icon = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.ic_profile_navigation)),
+                    isSelected = true,
+                    selectedColor = primaryLight,
+                    unSelectedColor = Color.Gray
+                )
+            )
+        )
     }
 }
