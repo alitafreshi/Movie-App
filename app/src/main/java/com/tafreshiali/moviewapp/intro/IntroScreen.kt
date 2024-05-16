a1037854ee13059718e83674de1b36cbc2717544
alitafreshi <alitafreshi123@yahoo.com>
2024-05-16 09:55:45 +0330

    Implement IntroSliderComponent

--- app/src/main/java/com/tafreshiali/moviewapp/intro/IntroScreen.kt
+++ app/src/main/java/com/tafreshiali/moviewapp/intro/IntroScreen.kt
@@ -9,9 +9,12 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -30,6 +33,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -37,10 +43,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorInt
 import com.tafreshiali.ui_kit.design_system.ui.theme.AppTheme
+import com.tafreshiali.ui_kit.design_system.ui.theme.backgroundLight
+import com.tafreshiali.ui_kit.design_system.ui.theme.grayscale10Light
 import com.tafreshiali.ui_kit.design_system.ui.theme.grayscale50Light
 import com.tafreshiali.ui_kit.design_system.ui.theme.grayscale70Light
 import com.tafreshiali.ui_kit.design_system.ui.theme.primaryLight
+import com.tafreshiali.ui_kit.design_system.ui.theme.secondaryLight
+import com.tafreshiali.ui_kit.design_system.ui.theme.surfaceVariantLight
 import com.tbuonomo.viewpagerdotsindicator.compose.DotsIndicator
 import com.tbuonomo.viewpagerdotsindicator.compose.model.DotGraphic
 import com.tbuonomo.viewpagerdotsindicator.compose.type.ShiftIndicatorType
@@ -68,7 +79,11 @@ fun IntroSliderComponent(modifier: Modifier = Modifier, items: List<IntroItem>) 
             modifier = Modifier
                 .padding(bottom = 16.dp), state = pagerState, userScrollEnabled = false
         ) { page ->
-            IntroItemComponent(item = items[page])
+
+            if (isLastPage) {
+                IntroLastItemComponent(item = items[page])
+            } else
+                IntroItemComponent(item = items[page])
         }
 
         Button(modifier = Modifier
@@ -124,6 +139,7 @@ fun IntroSliderComponent(modifier: Modifier = Modifier, items: List<IntroItem>) 
                 pagerState = pagerState
             )
         }
+
     }
 }
 
@@ -186,6 +202,58 @@ private fun IntroItemComponentPreview() {
     IntroItemComponent(
         item = IntroItem(
             imageRes = com.tafreshiali.ui_kit.R.drawable.intro_1,
+            title = "zdljfbnfljknkjldgbfdbfkbflkblfkbfjkb",
+            description = "zdfjkkjdfbjkdfkjdfkdjfbkjdfnbkjnasd;ldfb;dglmbldbaldklbklsmdkbblmblkgmbkfgmbkmblkbmlksfblkfmblfkbmlfkmblfkblskblkmmfglbkgfslkflkf"
+        )
+    )
+}
+
+
+@Composable
+private fun IntroLastItemComponent(modifier: Modifier = Modifier, item: IntroItem) {
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .aspectRatio(3 / 4f)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = surfaceVariantLight), contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = item.imageRes),
+                contentDescription = "intro_image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp)
+                    .graphicsLayer { translationY = 100f },
+                alignment = Alignment.TopCenter
+            )
+        }
+        Text(
+            text = item.title,
+            style = AppTheme.typography.h5Bold.copy(textAlign = TextAlign.Center),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 56.dp)
+        )
+        Text(
+            text = item.description,
+            style = AppTheme.typography.bodyMediumMedium.copy(
+                color = grayscale70Light,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+private fun IntroLastItemComponentPreview() {
+    IntroLastItemComponent(
+        item = IntroItem(
+            imageRes = com.tafreshiali.ui_kit.R.drawable.intro_3,
             title = "zdljfbnfljknkjldgbfdbfkbflkblfkbfjkb",
             description = "zdfjkkjdfbjkdfkjdfkdjfbkjdfnbkjnasd;ldfb;dglmbldbaldklbklsmdkbblmblkgmbkfgmbkmblkbmlksfblkfmblfkbmlfkmblfkblskblkmmfglbkgfslkflkf"
         )
